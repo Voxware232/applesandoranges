53b9ba97b7d6084b022a711bec2fd46b6e7f38b4
Voxware232 <Voxware232>
2022-10-11 18:40:30 +0100

    First Draft Code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,49 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+int main(){
+    float N,D;
+    while (1){
+        float x = rand () % (100) ;
+
+        x = x/50;
+        x = x -1;
+
+
+        float y = rand () % (100) ;
+
+        y = y/50;
+        y = y -1;
+
+        //printf("\n%.3f\t",x);
+        //printf("%.3f",y);
+
+        float point = sqrt((x*x)+(y*y));
+
+        if (point < 1){
+            //printf("\nYes");
+            N = N + 1;
+            printf("\n N - %d",N);
+        }
+        else
+        {
+
+            //printf("\nNo");
+        }
+        D = D +1;
+        printf("\nD - %d",D);
+
+        float pi =  (N/D) * 4;
+        printf("\n%.6f",pi);
+
+    }
+
+
+
+
+
+    return 0 ;
+
 }
